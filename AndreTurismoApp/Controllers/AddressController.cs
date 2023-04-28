df0c9b02013fc8c190afd4c87102b432ddd0c93e
= <andre.silva@hubdoctor.com.br>
2023-04-28 11:05:30 -0300

    Add Service Controller

---
+++ AndreTurismoApp/Controllers/AddressController.cs
@@ -0,0 +1,26 @@
+﻿using AndreTurismoApp.Models;
+using AndreTurismoApp.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AndreTurismoApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddressController : ControllerBase
+    {
+
+        private readonly AddressService _addressService;
+        public AddressController(AddressService addressService)
+        {
+            _addressService = addressService;
+        }
+
+
+        [HttpGet]
+        public async Task<List<Address>> GetAddress()
+        {
+            return await _addressService.GetAddress();
+        }
+    }
+}
---
+++ AndreTurismoApp/Services/AddressService.cs
@@ -0,0 +1,25 @@
+﻿using AndreTurismoApp.Models;
+using Newtonsoft.Json;
+
+namespace AndreTurismoApp.Services
+{
+    public class AddressService
+    {
+
+        static readonly HttpClient addressClient = new HttpClient();
+        public async Task<List<Address>> GetAddress()
+        {
+            try
+            {
+                HttpResponseMessage response = await AddressService.addressClient.GetAsync("https://localhost:7060/api/Addresses");
+                response.EnsureSuccessStatusCode();
+                string address = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<List<Address>>(address);
+            }
+            catch (HttpRequestException e)
+            {
+                throw;
+            }
+        }
+    }
+}
